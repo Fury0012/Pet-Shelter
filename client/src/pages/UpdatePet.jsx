1fa7964a5eb77d078c69f13cb4bd3ff907be7f95
Majd Belaidi <marklstrawn350@gmail.com>
2023-10-04 01:11:26 +0100

    fixed edit func

--- client/src/pages/UpdatePet.jsx
+++ client/src/pages/UpdatePet.jsx
@@ -15,9 +15,10 @@ const UpdatePet = () => {
 
     useEffect(() => {
 
-        axios.get(`http://localhost:5000pets/${id}`)
+        axios.get(`http://localhost:5000/pets/${id}`)
             .then(res => {
-                const petData = res.data; 
+                
+                const petData = res.data[0]; 
                 setPetName(petData.PetName);
                 setPetType(petData.PetType);
                 setPetDescription(petData.PetDescription);
@@ -56,41 +57,41 @@ const UpdatePet = () => {
         <div>
             <h1>Pet Shelter</h1>
             <h2>Update information about this pet</h2>
-            <form onSubmit={onSubmitHandler} style={{ display: "flex", justifyContent: "center" }}>
-                <div style={{ width: "40%" }}>
-                    <div>
-                        <label>PetName:</label>
-                        <input type="string" onChange={(e) => { setPetName(e.target.value) }} value={PetName} />
-                    </div>
-                    <div>
-                        <label>PetType:</label>
-                        <input type="string" onChange={(e) => { setPetType(e.target.value) }} value={PetType} />
-                    </div>
-                    <div>
-                        <label>PetDescription:</label>
-                        <input type="string" onChange={(e) => { setPetDescription(e.target.value) }} value={PetDescription} />
-                    </div>
-                </div>
-                <div style={{ width: "40%" }}>
-                    <div>
-                        <label>Skill 1 (optional):</label>
-                        <input type="string" onChange={(e) => { setSkill1(e.target.value) }} value={Skill1} />
-                    </div>
-                    <div>
-                        <label>Skill 2 (optional):</label>
-                        <input type="string" onChange={(e) => { setSkill2(e.target.value) }} value={Skill2} />
-                    </div>
-                    <div>
-                        <label>Skill 3 (optional):</label>
-                        <input type="string" onChange={(e) => { setSkill3(e.target.value) }} value={Skill3} />
-                    </div>
-                </div>
-                <div>
-                    <button style={{ border: "1px solid black", backgroundColor: "blue", boxShadow: "2px 2px 2px black", width: "120px", height: "30px" }}>
-                        <i className="bi bi-pen"></i> Update Pet
-                    </button>
-                </div>
-            </form>
+                <form onSubmit={onSubmitHandler} style={{ display: "flex", justifyContent: "center", gap: "20px" }}>
+    <div style={{ display: "flex", flexDirection: "column", width: "30%" }}>
+        <div className="input-wrapper">
+        <label htmlFor="PetName">Pet Name:</label>
+        <input type="text" id="PetName" onChange={(e) => setPetName(e.target.value)} value={PetName} />
+        </div>
+        <div className="input-wrapper">
+        <label htmlFor="PetType">Pet Type:</label>
+        <input type="text" id="PetType" onChange={(e) => setPetType(e.target.value)} value={PetType} />
+        </div>
+        <div className="input-wrapper">
+        <label htmlFor="PetDescription">Pet Description:</label>
+        <input type="text" id="PetDescription" onChange={(e) => setPetDescription(e.target.value)} value={PetDescription} />
+        </div>
+    </div>
+    <div style={{ display: "flex", flexDirection: "column", width: "30%" }}>
+        <div className="input-wrapper">
+        <label htmlFor="Skill1">Skill 1 (optional):</label>
+        <input type="text" id="Skill1" onChange={(e) => setSkill1(e.target.value)} value={Skill1} />
+        </div>
+        <div className="input-wrapper">
+        <label htmlFor="Skill2">Skill 2 (optional):</label>
+        <input type="text" id="Skill2" onChange={(e) => setSkill2(e.target.value)} value={Skill2} />
+        </div>
+        <div className="input-wrapper">
+        <label htmlFor="Skill3">Skill 3 (optional):</label>
+        <input type="text" id="Skill3" onChange={(e) => setSkill3(e.target.value)} value={Skill3} />
+        </div>
+    </div>
+    <div style={{ width: "20%", display: "flex", alignItems: "flex-end" }}>
+        <button style={{ border: "1px solid black", backgroundColor: "blue", boxShadow: "2px 2px 2px black", width: "120px", height: "40px", color: "white" }}>
+        <i className="bi bi-box-arrow-down"></i> Add Pet
+        </button>
+    </div>
+    </form>
         </div>
     );
 }
